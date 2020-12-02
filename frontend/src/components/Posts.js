3b924ed6bfb1548faa4d1eba0c8d1f91baaca677
RaktimParasar <raktimparasar96@gmail.com>
2020-12-02 10:44:29 +0530

    show add new post message if no post found

--- frontend/src/components/Posts.js
+++ frontend/src/components/Posts.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { getAllPosts } from '../actions/post';
 import { connect } from 'react-redux';
 import Moment from 'react-moment';
+import Spinner from './Spinner';
 
 const Posts = ({ getAllPosts, post: {posts, loading}}) => {
 
@@ -13,38 +14,53 @@ const Posts = ({ getAllPosts, post: {posts, loading}}) => {
     return (
         <Fragment>
             {
-                posts && posts.length > 0 &&
-                    posts.map(post => (
-                        <article key={post.id} className="post">
-                            <a className="post-link" href="/detail-post.html">
-                                <h2>{post.title}</h2>
-                            </a>
-                            <div className="post-item">
-                                <div>
-                                    <span><Moment format='MMMM Do, YYYY'>{post.timestamp}</Moment></span>
-                                    <span>11 min read</span>
-                                </div>
-                                <div>
-                                    <span>Author : {post.author}</span>
-                                    <span>Category : {post.category}</span>
-                                </div>
-                            </div>
-                            <p className="post-text">{post.body}</p>
-                            <div className="comment">
-                                <p>
-                                    {
-                                        post.commentCount > 1 ? "Comments" : "Comment"
-                                    }{' '}
-                                        <span className="dot">{post.commentCount}</span></p>
-                                <p>
-                                    {
-                                        post.voteScore > 1 ? "Votes" : "Vote"
-                                    }{' '}
-                                        <span className="dot">{post.voteScore}</span></p>
-                            </div>
-                        </article>
-                    ))
+                posts === null || loading ? <Spinner /> : <Fragment>
+                    {
+                        posts.length > 0 ? (
+                        <Fragment>
+                            {
+                                posts.map(post => (
+                                    <article key={post.id} className="post">
+                                        <a className="post-link" href="/detail-post.html">
+                                            <h2>{post.title}</h2>
+                                        </a>
+                                        <div className="post-item">
+                                            <div>
+                                                <span><Moment format='MMMM Do, YYYY'>{post.timestamp}</Moment></span>
+                                                <span>11 min read</span>
+                                            </div>
+                                            <div>
+                                                <span>Author : {post.author}</span>
+                                                <span>Category : {post.category}</span>
+                                            </div>
+                                        </div>
+                                        <p className="post-text">{post.body}</p>
+                                        <div className="comment">
+                                            <p>
+                                                {
+                                                    post.commentCount > 1 ? "Comments" : "Comment"
+                                                }{' '}
+                                                    <span className="dot">{post.commentCount}</span></p>
+                                            <p>
+                                                {
+                                                    post.voteScore > 1 ? "Votes" : "Vote"
+                                                }{' '}
+                                                    <span className="dot">{post.voteScore}</span></p>
+                                        </div>
+                                    </article>
+                        ))
+                            }
+                        </Fragment>
+                        ) : (
+                        <Fragment>
+                            <h4>No post yet , Create a new post</h4>
+                        </Fragment>
+                        )
+                            
             }
+                </Fragment>
+            }
+            
         </Fragment>
     )
 }
