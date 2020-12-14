d49fd99616a66f2604d8150362502e71b0a67f36
RaktimParasar <raktimparasar96@gmail.com>
2020-12-14 19:58:38 +0530

    ready to deploy

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ const path = require("path");
 const app = express();
 
 app.use(express.static("public"));
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+});
 app.use(cors());
 
 // app.get('/', (req, res) => {
