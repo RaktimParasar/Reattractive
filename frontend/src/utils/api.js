e15f8b4a822f80b7d2ae91d94949d718f808cb70
RaktimParasar <raktimparasar96@gmail.com>
2020-12-15 09:10:46 +0530

    adding env

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,18 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
 
 // Generate a unique token
-let token = localStorage.token
-if (!token)
-    token = localStorage.token = Math.random().toString(36).substr(-8);
+let token = localStorage.token;
+if (!token) token = localStorage.token = Math.random().toString(36).substr(-8);
 
-const url = "http://localhost:3001";
+const url = process.env.REACT_APP_API_SERVER;
 
 const api = axios.create({
-    baseURL: `${url}/`,
-    headers : {
-        'Accept': 'application/json',
-        'Authorization': token
-    }
-})
+  baseURL: `${url}`,
+  headers: {
+    Accept: "application/json",
+    Authorization: token,
+  },
+});
 
 export default api;
